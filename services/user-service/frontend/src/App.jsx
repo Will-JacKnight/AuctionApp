6ce9365aa7c2386df0a4df7f2ff5597e3e834bb9
yf724 <kristinafan@Kristinas-MacBook.local>
2025-02-12 15:55:08 +0000

    first version of login and signup

--- services/user-service/frontend/src/App.jsx
+++ services/user-service/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
+import Login from './pages /Login';
+import SignUp from './pages /SignUp';
 import './App.css';  // For styling
 
 function App() {
