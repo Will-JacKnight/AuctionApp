ffc914185aed1af9eff1c418a300813406bed184
Rody Huang <rh624@ic.ac.uk>
2025-02-13 11:54:56 +0000

    update requirement

--- services/user-service/app.py
+++ services/user-service/app.py
@@ -23,6 +23,8 @@ print(SUPABASE_URL)
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
+
+"""
 ### **User Registration API **
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -108,3 +110,4 @@ def protected():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000, debug=True)
+    """
