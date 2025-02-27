68b9ac25e43afda0764505f06b8beaa86981debb
Danny_Cheng <danny40263@gmail.com>
2025-02-27 00:37:13 +0000

    fix/debug_heroku_app_frontend_v1

--- services/frontend/vite.config.js
+++ services/frontend/vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
   server: {
     port: process.env.PORT || 5173, // Use Heroku's dynamic $PORT or default to 5173 for local development
     host: '0.0.0.0', // Ensure Vite listens on all network interfaces
+    allowedHosts: [
+      "bidding-app-sse-6fcb4edfed53.herokuapp.com" // Allow your Heroku domain
+    ],
   },
 });
  
