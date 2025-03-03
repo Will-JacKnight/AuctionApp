4a81430dbe2467a9ac2e705a70e7eff853e5f56a
Will Jacknight <jiankaiwang1999@163.com>
2025-03-03 14:30:44 +0000

    fix/api url injection

--- services/frontend/src/config.js
+++ services/frontend/src/config.js
@@ -1,5 +1,12 @@
 // Runtime configuration
-let API_URL = window.API_URL || import.meta.env.VITE_API_GATEWAY_LOCAL_URL;
+let API_URL;
+
+// Determine the API URL based on the runtime environment
+if (window.API_URL) {
+  API_URL = window.API_URL;
+} else {
+  API_URL = import.meta.env.VITE_API_GATEWAY_LOCAL_URL;
+}
 
 export const setApiUrl = (url) => {
   API_URL = url;
