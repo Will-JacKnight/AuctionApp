50db3f03dcb52a8cddac8b4caed578e8431f8473
Will Jacknight <jiankaiwang1999@163.com>
2025-03-03 20:05:10 +0000

    fix/revert port change

--- services/auction-service/app.py
+++ services/auction-service/app.py
@@ -31,5 +31,4 @@ app.register_blueprint(dashboard)
 app.register_blueprint(productPage)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 7070)) 
-    socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=7070, debug=True, allow_unsafe_werkzeug=True)
