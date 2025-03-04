b0b42e8e3de662a4cdedf0b05399cb0388bd6a17
yashkumar03 <ynk24@ic.ac.uk>
2025-03-04 12:52:25 +0000

    add logout functionality

--- services/frontend/src/components/NavBar.jsx
+++ services/frontend/src/components/NavBar.jsx
@@ -1,7 +1,19 @@
 import { NavLink } from 'react-router-dom';
 import "./../styles/navBar.css"
+import { useNavigate } from 'react-router-dom';
 
 function NavBar() {
+
+  const navigate  = useNavigate()
+  const  handleLogout = () => {
+    if(sessionStorage.getItem("token")) {
+      sessionStorage.removeItem("token")
+      alert(" logout Successfull!")
+      navigate("/")
+    }
+  }
+
+      
     return (
         <nav className="navbar">
         <h1>BiddingHub</h1>
@@ -9,6 +21,8 @@ function NavBar() {
           <li><NavLink to="/" className={({ isActive }) => (isActive ? "active-link" : "")}>Home</NavLink></li>
           <li><NavLink to="/listing" className={({ isActive }) => (isActive ? "active-link" : "")}>Sell</NavLink></li>
           <li><NavLink to="/dashboard" className={({ isActive }) => (isActive ? "active-link" : "")}>Dashboard</NavLink></li>
+
+          <img src="/images/logout.png" className='logout-icon' onClick={handleLogout}/>
         </ul>
       </nav>
     )
